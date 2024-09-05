7a397293f843717f899ea4eac8ea3bf5b59710dc
Alexandre <127530540+Alexandre1111111111@users.noreply.github.com>
2024-09-05 00:04:15 -0300

    Add files via upload

---
+++ estoque.js
@@ -0,0 +1,69 @@
+const mv = document.querySelector("#mv");
+const apr = document.querySelector("#apr");
+const ele = document.querySelector("#ele");
+const ex = document.querySelector("#ex");
+const moveis = document.querySelector(".moveis");
+
+mv.addEventListener("click", () => {
+    mv.style.color = "#deff65";
+    mv.style.borderColor = "#deff65";
+    apr.style.color = "#ffffff";
+    apr.style.borderColor = "#ffffff";
+    ele.style.color = "#ffffff";
+    ele.style.borderColor = "#ffffff";
+    ex.style.color = "#ffffff";
+    ex.style.borderColor = "#ffffff";
+    prim();
+});
+
+apr.addEventListener("click", () => {
+    apr.style.color = "#deff65";
+    apr.style.borderColor = "#deff65";
+    mv.style.color = "#ffffff";
+    mv.style.borderColor = "#ffffff";
+    ele.style.color = "#ffffff";
+    ele.style.borderColor = "#ffffff";
+    ex.style.color = "#ffffff";
+    ex.style.borderColor = "#ffffff";
+    seg();
+});
+
+ele.addEventListener("click", () => {
+    ele.style.color = "#deff65";
+    ele.style.borderColor = "#deff65";
+    mv.style.color = "#ffffff";
+    mv.style.borderColor = "#ffffff";
+    apr.style.color = "#ffffff";
+    apr.style.borderColor = "#ffffff";
+    ex.style.color = "#ffffff";
+    ex.style.borderColor = "#ffffff";
+    ter();
+});
+
+ex.addEventListener("click", () => {
+    ex.style.color = "#deff65";
+    ex.style.borderColor = "#deff65";
+    mv.style.color = "#ffffff";
+    mv.style.borderColor = "#ffffff";
+    apr.style.color = "#ffffff";
+    apr.style.borderColor = "#ffffff";
+    ele.style.color = "#ffffff";
+    ele.style.borderColor = "#ffffff";
+    qua();
+});
+
+function prim() {
+    moveis.style.display = "grid";
+}
+
+function seg() {
+    moveis.style.display = "none";
+}
+
+function ter() {
+    moveis.style.display = "none";
+}
+
+function qua() {
+    moveis.style.display = "none";
+}
